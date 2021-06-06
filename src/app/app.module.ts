5dae209d57ca340bb9d972cf735ded4b1e158541
thothieon <thothieon@hotmail.com>
2021-06-07 00:44:44 +0800

    製作代辦事項 參考網址 https://developer.mozilla.org/zh-TW/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Angular_todo_list_beginning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// 製作代辦事項 參考網址
+// https://developer.mozilla.org/zh-TW/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Angular_todo_list_beginning
+
 @NgModule({
   declarations: [
     AppComponent
