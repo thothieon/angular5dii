1b52e10be47e331544501154a37a2f37307bc412
thothieon <thothieon@hotmail.com>
2021-11-10 18:40:35 +0800

    特殊功能網頁 一頁式網頁，讓看IG FB的客人連回官網，把人導回官網

---
+++ src/app/pages/linkidiving/linkidiving.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-linkidiving',
+  templateUrl: './linkidiving.component.html',
+  styleUrls: ['./linkidiving.component.scss']
+})
+export class LinkidivingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
