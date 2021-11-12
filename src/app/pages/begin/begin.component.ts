b9b4030f821ea46e6072c33e0c7d8c9a8dbba392
thothieon <thothieon@hotmail.com>
2021-11-12 14:49:35 +0800

    使用Firebase firestore裡面的資料顯示在首頁上面 課程期別 - iDiving/course/keycourse 出團期別 - iDiving/activity/keyactivity

--- src/app/pages/begin/begin.component.ts
+++ src/app/pages/begin/begin.component.ts
@@ -1,33 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Course { 
+  id: string;
+  first: string; 
+  last: string; 
+  handle: string; 
+}
+
+export interface Activity { 
+  id: string;
+  first: string; 
+  last: string; 
+  handle: string; 
+}
+
 @Component({
   selector: 'app-begin',
   templateUrl: './begin.component.html',
   styleUrls: ['./begin.component.scss']
 })
 export class BeginComponent implements OnInit {
-  
-  elementsCourse: any = [
-    {id: 1, first: '自由潛水(冬季班)-04期', last: '10-26', handle: '可報名'},
-    {id: 2, first: '自由潛水(夏季假日)-01期', last: '05-03', handle: '可報名'},
-    {id: 3, first: '初級潛水(冬季班)-03期', last: '10-27', handle: '可報名'},
-    {id: 4, first: '初級潛水(夏季假日)-01期', last: '04-12', handle: '可報名'},
-    {id: 5, first: '高氧專長-16期', last: '10-26', handle: '可報名'}
-  ];
 
-  elementsActivity: any = [
-    {id: 1, first: '小琉球', last: '2021-10-30', handle: '剩1位'},
-    {id: 2, first: '小琉球', last: '2021-11-02', handle: '可報名'},
-    {id: 3, first: '墾丁', last: '2021-11-20', handle: '可報名'},
-    {id: 4, first: '墾丁', last: '2021-12-18', handle: '可報名'},
-    {id: 5, first: '小琉球(自潛)', last: '2021-03-18', handle: '可報名'}
-  ];
-
-  headElementsCourse = ['ID', 'First', 'Last', 'Handle'];
-
-  headElementsActivity = ['ID', 'First', 'Last', 'Handle'];
-
-  constructor() { }
+  private coursesCollection: AngularFirestoreCollection<Course>;
+  courses: Observable<Course[]>;
+  private activitysCollection: AngularFirestoreCollection<Activity>;
+  activitys: Observable<Activity[]>;
+  constructor(private afs: AngularFirestore) {
+    this.coursesCollection = afs.collection<Course>('iDiving/course/keycourse');
+    this.courses = this.coursesCollection.valueChanges();
+    this.activitysCollection = afs.collection<Activity>('iDiving/activity/keyactivity');
+    this.activitys = this.activitysCollection.valueChanges();
+  }
+  addItem(course: Course, activitys: Activity) {
+    this.coursesCollection.add(course);
+    this.activitysCollection.add(activitys);
+  }
 
   ngOnInit(): void {
   }
