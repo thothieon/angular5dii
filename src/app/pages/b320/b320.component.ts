7dd3aca8333f4baa9d92b88d912860879dbe60f2
thothieon <thothieon@hotmail.com>
2022-01-11 15:28:15 +0800

    新增 器材 重裝 的頁面 代號b320 II

---
+++ src/app/pages/b320/b320.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-b320',
+  templateUrl: './b320.component.html',
+  styleUrls: ['./b320.component.scss']
+})
+export class B320Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
