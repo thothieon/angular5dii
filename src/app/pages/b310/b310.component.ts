12ef7110418c9c3b616e2a5ed88ca83faaff4fae
thothieon <thothieon@hotmail.com>
2022-03-02 16:05:19 +0800

    修改輕重裝排版

--- src/app/pages/b310/b310.component.ts
+++ src/app/pages/b310/b310.component.ts
@@ -9,26 +9,66 @@ export class B310Component implements OnInit {
 
   // 新增屬性及內容
   stands = [
-    { img:'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
-    hrf:'https://www.facebook.com/janicewa2002',name:'TUSA', title:'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片' },
-    { img:'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
-    hrf:'https://www.facebook.com/janicewa2002',name:'TUSA', title:'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片' },
-    { img:'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
-    hrf:'https://www.facebook.com/janicewa2002',name:'TUSA', title:'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片' },
-    { img:'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
-    hrf:'https://www.facebook.com/janicewa2002',name:'TUSA', title:'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片' },
-    { img:'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
-    hrf:'https://www.facebook.com/janicewa2002',name:'TUSA', title:'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片' },
-    { img:'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
-    hrf:'https://www.facebook.com/janicewa2002',name:'TUSA', title:'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片' },
-    { img:'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
-    hrf:'https://www.facebook.com/janicewa2002',name:'TUSA', title:'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片' },
-    { img:'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
-    hrf:'https://www.facebook.com/janicewa2002',name:'TUSA', title:'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片' },
-    { img:'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
-    hrf:'https://www.facebook.com/janicewa2002',name:'TUSA', title:'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片' },
-    { img:'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
-    hrf:'https://www.facebook.com/janicewa2002',name:'TUSA', title:'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片' }
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
+      name: 'TUSA', 
+      price: '70',
+      title: 'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
+      name: 'TUSA', 
+      price: '70',
+      title: 'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
+      name: 'TUSA', 
+      price: '70',
+      title: 'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
+      name: 'TUSA', 
+      price: '70',
+      title: 'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
+      name: 'TUSA', 
+      price: '70',
+      title: 'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
+      name: 'TUSA', 
+      price: '70',
+      title: 'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
+      name: 'TUSA', 
+      price: '70',
+      title: 'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
+      name: 'TUSA', 
+      price: '70',
+      title: 'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
+      name: 'TUSA', 
+      price: '70',
+      title: 'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1_aUKzW9vgOZqqRt3VphwSOa1ilXZekgs',
+      name: 'TUSA', 
+      price: '70',
+      title: 'M212 雙面鏡 （水肺）\n特色：Freedom任意貼裙邊更加貼合臉型\n專利3D立體面鏡帶舒適不易滑落\n雙面鏡設計可更換近視鏡片'
+    }
   ]
 
   constructor() { }
--- src/app/pages/b320/b320.component.ts
+++ src/app/pages/b320/b320.component.ts
@@ -7,6 +7,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class B320Component implements OnInit {
 
+  // 新增屬性及內容
+  stands = [
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1DymrwxurBFpIVrwBNDaRfyvlaE909Yax',
+      name: 'Aqualung PRO HD BCD', 
+      price: '17,500',
+      title: 'Pro HD配備整合式配重系統，簡單一拉就能\n將配重袋鬆脫，配重袋裝填只需插入口袋\n即可”卡住”。'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1DymrwxurBFpIVrwBNDaRfyvlaE909Yax',
+      name: 'Aqualung PRO HD BCD', 
+      price: '17,500',
+      title: 'Pro HD配備整合式配重系統，簡單一拉就能\n將配重袋鬆脫，配重袋裝填只需插入口袋\n即可”卡住”。'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1DymrwxurBFpIVrwBNDaRfyvlaE909Yax',
+      name: 'Aqualung PRO HD BCD', 
+      price: '17,500',
+      title: 'Pro HD配備整合式配重系統，簡單一拉就能\n將配重袋鬆脫，配重袋裝填只需插入口袋\n即可”卡住”。'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1DymrwxurBFpIVrwBNDaRfyvlaE909Yax',
+      name: 'Aqualung PRO HD BCD', 
+      price: '17,500',
+      title: 'Pro HD配備整合式配重系統，簡單一拉就能\n將配重袋鬆脫，配重袋裝填只需插入口袋\n即可”卡住”。'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1DymrwxurBFpIVrwBNDaRfyvlaE909Yax',
+      name: 'Aqualung PRO HD BCD', 
+      price: '17,500',
+      title: 'Pro HD配備整合式配重系統，簡單一拉就能\n將配重袋鬆脫，配重袋裝填只需插入口袋\n即可”卡住”。'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1DymrwxurBFpIVrwBNDaRfyvlaE909Yax',
+      name: 'Aqualung PRO HD BCD', 
+      price: '17,500',
+      title: 'Pro HD配備整合式配重系統，簡單一拉就能\n將配重袋鬆脫，配重袋裝填只需插入口袋\n即可”卡住”。'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1DymrwxurBFpIVrwBNDaRfyvlaE909Yax',
+      name: 'Aqualung PRO HD BCD', 
+      price: '17,500',
+      title: 'Pro HD配備整合式配重系統，簡單一拉就能\n將配重袋鬆脫，配重袋裝填只需插入口袋\n即可”卡住”。'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1DymrwxurBFpIVrwBNDaRfyvlaE909Yax',
+      name: 'Aqualung PRO HD BCD', 
+      price: '17,500',
+      title: 'Pro HD配備整合式配重系統，簡單一拉就能\n將配重袋鬆脫，配重袋裝填只需插入口袋\n即可”卡住”。'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1DymrwxurBFpIVrwBNDaRfyvlaE909Yax',
+      name: 'Aqualung PRO HD BCD', 
+      price: '17,500',
+      title: 'Pro HD配備整合式配重系統，簡單一拉就能將配重袋鬆脫，配重袋裝填只需插入口袋即可”卡住”。'
+    },
+    {
+      img: 'https://drive.google.com/uc?export=view&id=1DymrwxurBFpIVrwBNDaRfyvlaE909Yax',
+      name: 'Aqualung PRO HD BCD', 
+      price: '17,500',
+      title: 'Pro HD配備整合式配重系統，簡單一拉就能\n將配重袋鬆脫，配重袋裝填只需插入口袋\n即可”卡住”。'
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
