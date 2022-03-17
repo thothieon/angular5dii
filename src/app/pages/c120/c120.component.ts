7c5f8a4c440a0ac20077351ca11102fef60659a4
thothieon <thothieon@hotmail.com>
2022-03-17 22:05:36 +0800

    OWD 重新排版

--- src/app/pages/c120/c120.component.ts
+++ src/app/pages/c120/c120.component.ts
@@ -8,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class C120Component implements OnInit {
 
   active = 1;
+  
+  Weekendcards = [
+    {img: '../../../../assets/Image/Data/OWD_Weekend_Schedule_20221.png'},
+    {img: '../../../../assets/Image/Data/OWD_Weekend_Schedule_20222.png'},
+  ];
+  
+  Weekdayscards = [
+    {img: '../../../../assets/Image/Data/OWD_Weekdays_Schedule_20221.png'},
+    {img: '../../../../assets/Image/Data/OWD_Weekdays_Schedule_20222.png'},
+  ];
 
   constructor() { }
 
