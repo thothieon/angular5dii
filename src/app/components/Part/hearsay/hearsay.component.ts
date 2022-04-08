2ce3e3e83762a06d7875c69d9532fadaf965398e
thothieon <thothieon@hotmail.com>
2022-04-08 20:36:16 +0800

    新增最新消息與後端表格(Firestore)連線

--- src/app/components/Part/hearsay/hearsay.component.ts
+++ src/app/components/Part/hearsay/hearsay.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Hearsay { 
+  id: string;
+  color: string; 
+  title: string; 
+  date: string; 
+  item: string; 
+}
+
 @Component({
   selector: 'app-hearsay',
   templateUrl: './hearsay.component.html',
@@ -7,7 +18,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HearsayComponent implements OnInit {
 
-  constructor() { }
+  private hearsaysCollection: AngularFirestoreCollection<Hearsay>;
+  hearsays: Observable<Hearsay[]>;
+  constructor(private afs: AngularFirestore) {
+    this.hearsaysCollection = afs.collection<Hearsay>('iDiving/begin/hearsay');
+    this.hearsays = this.hearsaysCollection.valueChanges();
+  }
+  addItem(hearsays: Hearsay) {
+    this.hearsaysCollection.add(hearsays);
+  }
+
 
   ngOnInit(): void {
   }
--- src/app/pages/m100/m100.component.ts
+++ src/app/pages/m100/m100.component.ts
@@ -2,47 +2,45 @@ import { MdbTablePaginationComponent, MdbTableDirective } from 'angular-bootstra
 
 import { Component, OnInit, ViewChild, HostListener, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Hearsaytotal { 
+  id: string;
+  color: string; 
+  title: string; 
+  date: string; 
+  item: string; 
+  img: string; 
+  detail: string; 
+}
+
 @Component({
   selector: 'app-m100',
   templateUrl: './m100.component.html',
   styleUrls: ['./m100.component.scss']
 })
 export class M100Component implements OnInit, AfterViewInit {
+
   @ViewChild(MdbTablePaginationComponent, { static: true }) mdbTablePagination: MdbTablePaginationComponent;
   @ViewChild(MdbTableDirective, { static: true }) mdbTable: MdbTableDirective
-  elements: any = [];
+
   previous: any = [];
-  headElements = ['ID', 'First', 'Last', 'Handle'];
 
-  elementsa: any = [
-    {id: 1, img: '../../../../assets/Image/Data/綠島自潛深度團.jpg', item:'訊息', data:'2022.02.24', last: 'Otto', text: '綠島自潛深度團'},
-    {id: 2, img: '../../../../assets/Image/Data/海洋博物誌2.jpg', item:'訊息', data:'2022.02.22', last: 'Thornton', text: '海洋博物誌 新書分享會，於2/26(六)15:00開始'},
-    {id: 3, img: '../../../../assets/Image/Data/22新年壓歲金_n.jpg', item:'優惠', data:'2022.02.23', last: 'the Bird', text: '2022 OWD FD 課程夏季班早鳥優惠'},
-    {id: 4, img: '../../../../assets/Image/Data/綠島自潛深度團.jpg', item:'訊息', data:'2022.02.24', last: 'Otto', text: '綠島自潛深度團'},
-    {id: 5, img: '../../../../assets/Image/Data/海洋博物誌2.jpg', item:'訊息', data:'2022.02.24', last: 'Thornton', text: '海洋博物誌 新書分享會，於2/26(六)15:00開始'},
-    {id: 6, img: '../../../../assets/Image/Data/22新年壓歲金_n.jpg', item:'優惠', data:'2022.02.24', last: 'the Bird', text: '2022 OWD FD 課程夏季班早鳥優惠'},
-    {id: 7, img: '../../../../assets/Image/Data/綠島自潛深度團.jpg', item:'訊息', data:'2022.02.24', last: 'Otto', text: '綠島自潛深度團'},
-    {id: 8, img: '../../../../assets/Image/Data/海洋博物誌2.jpg', item:'訊息', data:'2022.02.24', last: 'Thornton', text: '海洋博物誌 新書分享會，於2/26(六)15:00開始'},
-    {id: 9, img: '../../../../assets/Image/Data/22新年壓歲金_n.jpg', item:'優惠', data:'2022.02.24', last: 'the Bird', text: '2022 OWD FD 課程夏季班早鳥優惠'},
-    {id: 10, img: '../../../../assets/Image/Data/綠島自潛深度團.jpg', item:'訊息', data:'2022.02.24', last: 'Otto', text: '綠島自潛深度團'},
-    {id: 11, img: '../../../../assets/Image/Data/海洋博物誌2.jpg', item:'訊息', data:'2022.02.24', last: 'Thornton', text: '海洋博物誌 新書分享會，於2/26(六)15:00開始'},
-    {id: 12, img: '../../../../assets/Image/Data/22新年壓歲金_n.jpg', item:'優惠', data:'2022.02.24', last: 'the Bird', hatextndle: '2022 OWD FD 課程夏季班早鳥優惠'},
-    {id: 13, img: '../../../../assets/Image/Data/綠島自潛深度團.jpg', item:'訊息', data:'2022.02.24', last: 'Otto', text: '綠島自潛深度團'},
-    {id: 14, img: '../../../../assets/Image/Data/海洋博物誌2.jpg', item:'訊息', data:'2022.02.24', last: 'Thornton', text: '海洋博物誌 新書分享會，於2/26(六)15:00開始'},
-    {id: 15, img: '../../../../assets/Image/Data/22新年壓歲金_n.jpg', item:'優惠', data:'2022.02.24', last: 'the Bird', text: '2022 OWD FD 課程夏季班早鳥優惠'},
-  ];
+  private hearsaytotalsCollection: AngularFirestoreCollection<Hearsaytotal>;
+  hearsaytotals: Observable<Hearsaytotal[]>;
+  constructor(private cdRef: ChangeDetectorRef, private afs: AngularFirestore) {
+    this.hearsaytotalsCollection = afs.collection<Hearsaytotal>('iDiving/begin/hearsaytotal');
+    this.hearsaytotals = this.hearsaytotalsCollection.valueChanges();
+  }
 
-  headElementsa = ['ID', 'img', 'Last', 'Handle'];
-
-  constructor(private cdRef: ChangeDetectorRef) { }
+  addItem(hearsaytotals: Hearsaytotal) {
+    this.hearsaytotalsCollection.add(hearsaytotals);
+  }
 
   ngOnInit(): void {
-    /*for (let i = 1; i <= 15; i++) {
-      this.elements.push({id: i.toString(), first: 'User ' + i, last: 'Name ' + i, handle: 'Handle ' + i});
-    }*/
-
-    this.mdbTable.setDataSource(this.elementsa);
-    this.elementsa = this.mdbTable.getDataSource();
+    this.mdbTable.setDataSource(this.hearsaytotals);
+    this.hearsaytotals = this.mdbTable.getDataSource();
     this.previous = this.mdbTable.getDataSource();
   }
 
