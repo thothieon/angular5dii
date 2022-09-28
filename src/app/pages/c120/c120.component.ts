0863ce311f9dfdab679d6d1ca5bd1d89108aa56a
thothieon <thothieon@hotmail.com>
2022-09-28 23:15:39 +0800

    更換 *OWD假日班 2023年課程期別圖 *OWD平日班 2023年課程期別圖

--- src/app/pages/c120/c120.component.ts
+++ src/app/pages/c120/c120.component.ts
@@ -11,14 +11,12 @@ export class C120Component implements OnInit {
   
   //假日班
   Weekenders = [
-    //{img: '../../../../assets/Image/Data/OWD_Weekend_Schedule_20221.png'},
-    {img: '../../../../assets/Image/Data/OWD_Weekend_Schedule_20222.png'},
+    {img: '../../../../assets/Image/Data/OWD_Weekend_Schedule_2023.png'}
   ];
   
   //平日班
   Weekdays = [
-    //{img: '../../../../assets/Image/Data/OWD_Weekdays_Schedule_20221.png'},
-    {img: '../../../../assets/Image/Data/OWD_Weekdays_Schedule_20222.png'},
+    {img: '../../../../assets/Image/Data/OWD_Weekdays_Schedule_2023.png'},
   ];
 
   constructor() { }
