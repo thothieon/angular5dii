e57997c7848650228684192658c1a8547dd3fe24
thothieon <thothieon@hotmail.com>
2023-04-28 17:07:01 +0800

    美人魚 親子美人魚 排版更新

--- src/app/pages/cfd130/cfd130.component.ts
+++ src/app/pages/cfd130/cfd130.component.ts
@@ -9,6 +9,12 @@ export class Cfd130Component implements OnInit {
 
   active = 1;
 
+  //美人魚 平日 假日
+  Weekdays = [
+    {img: '../../../../assets/Image/Data/FD_MM_01.png'},
+    {img: '../../../../assets/Image/Data/FD_MM_02.png'}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
