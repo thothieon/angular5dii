f8583505f237a3e1d13d864934c943d38466aa48
thothieon <thothieon@hotmail.com>
2023-10-01 22:09:23 +0800

    新增賣活動為主的主要頁面

---
+++ src/app/modules/activity/pages/activity-main/activity-main.component.ts
@@ -0,0 +1,102 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-main',
+  templateUrl: './activity-main.component.html',
+  styleUrls: ['./activity-main.component.scss']
+})
+export class ActivityMainComponent {
+  
+  stands = [
+    {
+      img: '../../../assets/Image/DataActivity/S__68460924.jpg',
+      hrf: 'https://www.facebook.com/janicewa2002',
+      name: '綠島', 
+      title: '綠島原名火燒島，乃一火山集塊岩構成的島嶼，四周為裙狀珊瑚礁所環繞，位於台東東南方33公里海面上，面積約16平方公里環島長約20公里。綠島土質為棕紅壤土', 
+      title01: 'NT$', 
+      title02: ''
+    },
+    {
+      img: '../../../assets/Image/DataActivity/S__68460926.jpg',
+      hrf: 'https://www.facebook.com/janicewa2002',
+      name: '綠島', 
+      title: '綠島原名火燒島，乃一火山集塊岩構成的島嶼，四周為裙狀珊瑚礁所環繞，位於台東東南方33公里海面上，面積約16平方公里環島長約20公里。綠島土質為棕紅壤土', 
+      title01: 'NT$', 
+      title02: ''
+    },
+    {
+      img: '../../../assets/Image/DataActivity/S__68460927.jpg',
+      hrf: 'https://www.facebook.com/janicewa2002',
+      name: '綠島', 
+      title: '綠島原名火燒島，乃一火山集塊岩構成的島嶼，四周為裙狀珊瑚礁所環繞，位於台東東南方33公里海面上，面積約16平方公里環島長約20公里。綠島土質為棕紅壤土', 
+      title01: 'NT$', 
+      title02: ''
+    },
+    {
+      img: '../../../assets/Image/DataActivity/S__68460928.jpg',
+      hrf: 'https://www.facebook.com/janicewa2002',
+      name: '綠島', 
+      title: '綠島原名火燒島，乃一火山集塊岩構成的島嶼，四周為裙狀珊瑚礁所環繞，位於台東東南方33公里海面上，面積約16平方公里環島長約20公里。綠島土質為棕紅壤土', 
+      title01: 'NT$', 
+      title02: ''
+    },
+    {
+      img: '../../../assets/Image/DataActivity/S__68460928.jpg',
+      hrf: 'https://www.facebook.com/janicewa2002',
+      name: '綠島', 
+      title: '綠島原名火燒島，乃一火山集塊岩構成的島嶼，四周為裙狀珊瑚礁所環繞，位於台東東南方33公里海面上，面積約16平方公里環島長約20公里。綠島土質為棕紅壤土', 
+      title01: 'NT$', 
+      title02: ''
+    },
+    {
+      img: '../../../assets/Image/DataActivity/S__68460928.jpg',
+      hrf: 'https://www.facebook.com/janicewa2002',
+      name: '綠島', 
+      title: '綠島原名火燒島，乃一火山集塊岩構成的島嶼，四周為裙狀珊瑚礁所環繞，位於台東東南方33公里海面上，面積約16平方公里環島長約20公里。綠島土質為棕紅壤土', 
+      title01: 'NT$', 
+      title02: ''
+    },
+    {
+      img: '../../../assets/Image/DataActivity/S__68460929.jpg',
+      hrf: 'https://www.facebook.com/janicewa2002',
+      name: '綠島', 
+      title: '綠島原名火燒島，乃一火山集塊岩構成的島嶼，四周為裙狀珊瑚礁所環繞，位於台東東南方33公里海面上，面積約16平方公里環島長約20公里。綠島土質為棕紅壤土', 
+      title01: 'NT$', 
+      title02: ''
+    }
+  ]
+
+  stands01 = [
+    {
+      img: '../../../assets/Image/Coach/237.jpg',
+      hrf: 'https://www.facebook.com/janicewa2002',
+      name: '麗瑜', title: '', title01: '', title02: 'SSI 自由潛水 LV1 教練'
+    },
+    {
+      img: '../../../assets/Image/Coach/233.jpg',
+      hrf: 'https://www.facebook.com/profile.php?id=100004661648909',
+      name: 'Kayla', title: 'SSI 開放水域潛水員教練', title01: 'SSI 自由潛水 LV1 教練', title03: 'SSI 美人魚教練'
+    },
+    {
+      img: '../../../assets/Image/Coach/236.jpg',
+      hrf: 'https://www.facebook.com/xueqian91',
+      name: '薛', title: '', title01: 'SSI 自由潛水 LV1 教練', title03: ''
+    },
+    {
+      img: '../../../assets/Image/Coach/236.jpg',
+      hrf: 'https://www.facebook.com/xueqian91',
+      name: '薛', title: '', title01: 'SSI 自由潛水 LV1 教練', title03: ''
+    },
+    {
+      img: '../../../assets/Image/Coach/236.jpg',
+      hrf: 'https://www.facebook.com/xueqian91',
+      name: '薛', title: '', title01: 'SSI 自由潛水 LV1 教練', title03: ''
+    },
+    {
+      img: '../../../assets/Image/Coach/235.jpg',
+      hrf: 'https://www.facebook.com/profile.php?id=100000343460900',
+      name: '江江', title: 'SSI 自由潛水 LV1 教練', title01: '', title03: ''
+    }
+  ]
+
+}
