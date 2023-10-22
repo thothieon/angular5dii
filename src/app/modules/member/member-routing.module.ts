ebd18e9b6904c4ce3b0565bdb8b5eddd9fc93709
thothieon <thothieon@hotmail.com>
2023-10-23 00:02:56 +0800

    新增會員資訊編輯頁面

--- src/app/modules/member/member-routing.module.ts
+++ src/app/modules/member/member-routing.module.ts
@@ -3,8 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { MemberSearchComponent } from './pages/member-search/member-search.component';
 import { MemberNewComponent } from './pages/member-new/member-new.component';
+import { MemberInforComponent } from './pages/member-infor/member-infor.component';
 
 const routes: Routes = [
+  { path: 'infor/:id', component: MemberInforComponent },
+  { path: 'infor', component: MemberInforComponent },
   { path: 'new', component: MemberNewComponent },
   { path: '', component: MemberSearchComponent }
 ];
