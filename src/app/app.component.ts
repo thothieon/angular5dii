647f2b675b23e7e55c8b8490395459a13c716777
thothieon <thothieon@hotmail.com>
2024-01-07 17:35:04 +0800

    增加  設置文檔的字符編碼

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,6 +28,8 @@ export class AppComponent {
     //https://blog.poychang.net/how-to-use-html-head/
     this.meta.addTags([
           {name: 'title', content: '北部潛水課程首選【iDiving 全方位潛水中心】，精緻小班教學，提供保證班潛水課程｜水肺潛水｜ 自由潛水 ｜技術潛水｜ 美人魚課程 ｜生態潛水｜體驗潛水｜各項教練班課程 ｜潛水旅遊 ｜潛水器材裝備販售｜企業合作｜。'},
+          // 設置文檔的字符編碼
+          {name: 'charset', content: 'UTF-8'},
           //{name: 'description', content: 'How to use Angular 4 meta service'},
           //{name: 'author', content: 'talkingdotnet'},
           //{name: 'keywords', content: 'Angular, Meta Service'}
