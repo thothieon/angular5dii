1f03332a692c99fc199f13e32e72649469c558ba
thothieon <thothieon@hotmail.com>
2025-05-17 14:46:37 +0800

    c310專長潛水的頁面確認

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,10 @@ import { C101Component } from './pages/c101/c101.component';
 import { C120Component } from './pages/c120/c120.component';
 import { C130Component } from './pages/c130/c130.component';
 import { C210Component } from './pages/c210/c210.component';
+import { C310Component } from './pages/c310/c310.component';
 
 export const routes: Routes = [
+    { path: 'c310', component: C310Component },
     { path: 'c210', component: C210Component },
     { path: 'c130', component: C130Component },
     { path: 'c130', component: C130Component },
--- src/app/pages/c310/c310.component.ts
+++ src/app/pages/c310/c310.component.ts
@@ -1,7 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { FootComponent } from '../../components/foot/foot.component';
+import { HeadComponent } from '../../components/head/head.component';
+import { PaymentmethodComponent } from '../../components/paymentmethod/paymentmethod.component';
+
 
 @Component({
   selector: 'app-c310',
+  standalone: true,
+  imports: [
+    CommonModule,
+    NgbNavModule,
+    HeadComponent,
+    FootComponent,
+    PaymentmethodComponent
+  ],
   templateUrl: './c310.component.html',
   styleUrls: ['./c310.component.scss']
 })
@@ -14,18 +30,18 @@ export class C310Component implements OnInit {
 
   // DD 課表
   //DDSchedules = [
-    // 假日班
-    //{img: '../../../../assets/Image/Data/DD_Weekend_Schedule.png'},
-    // 平日班
-    //{img: '../../../../assets/Image/Data/DD_Weekdays_Schedule.png'}
+  // 假日班
+  //{img: '../../../../assets/Image/Data/DD_Weekend_Schedule.png'},
+  // 平日班
+  //{img: '../../../../assets/Image/Data/DD_Weekdays_Schedule.png'}
   //];
 
   // NL 課表
   //NLSchedules = [
-    // 假日班
-    //{img: '../../../../assets/Image/Data/NL_Weekend_Schedule.png'},
-    // 平日班
-    //{img: '../../../../assets/Image/Data/NL_Weekdays_Schedule.png'}
+  // 假日班
+  //{img: '../../../../assets/Image/Data/NL_Weekend_Schedule.png'},
+  // 平日班
+  //{img: '../../../../assets/Image/Data/NL_Weekdays_Schedule.png'}
   //];
 
   constructor() { }
