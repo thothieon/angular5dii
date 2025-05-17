71a935efb91b3482158fe54dd270d479b13c0db0
thothieon <thothieon@hotmail.com>
2025-05-17 13:42:29 +0800

    c120初階水肺課程網頁確認

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 
 import { BeginComponent } from './pages/begin/begin.component';
 import { C101Component } from './pages/c101/c101.component';
+import { C120Component } from './pages/c120/c120.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+    { path: 'c120', component: C120Component },
     { path: 'c101', component: C101Component },
     { path: '', component: BeginComponent }
 ];
